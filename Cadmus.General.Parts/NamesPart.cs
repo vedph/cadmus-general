e3b3f9970c9e206f37b08a49d5d77127f4b59e70
Myrmex <dfusi@hotmail.com>
2022-01-09 12:24:04 +0100

    Added NamesPart

---
+++ Cadmus.General.Parts/NamesPart.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Cadmus.Core;
+using Cadmus.Refs.Bricks;
+using Fusi.Tools.Config;
+
+namespace Cadmus.General.Parts
+{
+    /// <summary>
+    /// Names part. This contains any number of names assigned to the item.
+    /// <para>Tag: <c>it.vedph.names</c>.</para>
+    /// </summary>
+    [Tag("it.vedph.names")]
+    public sealed class NamesPart : PartBase
+    {
+        /// <summary>
+        /// Gets or sets the names.
+        /// </summary>
+        public List<AssertedProperName> Names { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NamesPart"/> class.
+        /// </summary>
+        public NamesPart()
+        {
+            Names = new List<AssertedProperName>();
+        }
+
+        /// <summary>
+        /// Get all the key=value pairs (pins) exposed by the implementor.
+        /// </summary>
+        /// <param name="item">The optional item. The item with its parts
+        /// can optionally be passed to this method for those parts requiring
+        /// to access further data.</param>
+        /// <returns>The pins: <c>tot-count</c> and a collection of pins with
+        /// these keys: <c>name</c>.</returns>
+        public override IEnumerable<DataPin> GetDataPins(IItem item = null)
+        {
+            DataPinBuilder builder = new DataPinBuilder(DataPinHelper.DefaultFilter);
+
+            builder.Set("tot", Names?.Count ?? 0, false);
+
+            if (Names?.Count > 0)
+            {
+                foreach (AssertedProperName name in Names)
+                {
+                    string n = name.Value?.GetFullName();
+                    if (!string.IsNullOrEmpty(n))
+                        builder.AddValue("name", n, filter: true);
+                }
+            }
+
+            return builder.Build(this);
+        }
+
+        /// <summary>
+        /// Gets the definitions of data pins used by the implementor.
+        /// </summary>
+        /// <returns>Data pins definitions.</returns>
+        public override IList<DataPinDefinition> GetDataPinDefinitions()
+        {
+            return new List<DataPinDefinition>(new[]
+            {
+                new DataPinDefinition(DataPinValueType.Integer,
+                   "tot-count",
+                   "The total count of names."),
+                new DataPinDefinition(DataPinValueType.String,
+                   "name",
+                   "The filtered names."),
+            });
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[Names]");
+
+            if (Names?.Count > 0)
+            {
+                sb.Append(' ');
+                int n = 0;
+                foreach (var entry in Names)
+                {
+                    if (++n > 3) break;
+                    if (n > 1) sb.Append("; ");
+                    sb.Append(entry);
+                }
+                if (Names.Count > 3)
+                    sb.Append("...(").Append(Names.Count).Append(')');
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cadmus.Seed.General.Parts/NamesPartSeeder.cs
@@ -0,0 +1,70 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.General.Parts;
+using Cadmus.Refs.Bricks;
+using Fusi.Tools.Config;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Seed.General.Parts
+{
+    /// <summary>
+    /// Seeder for <see cref="NamesPart"/>.
+    /// Tag: <c>seed.it.vedph.names</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.names")]
+    public sealed class NamesPartSeeder : PartSeederBase
+    {
+        private static List<AssertedProperName> GetNames(int count)
+        {
+            List<AssertedProperName> names = new List<AssertedProperName>();
+            for (int n = 1; n <= count; n++)
+            {
+                names.Add(new Faker<AssertedProperName>()
+                    .RuleFor(a => a.Value, f => new ProperName
+                    {
+                        Language = "eng",
+                        Pieces = new List<ProperNamePiece>
+                        {
+                            new ProperNamePiece
+                            {
+                                Type = "first",
+                                Value = f.Name.FirstName()
+                            },
+                            new ProperNamePiece
+                            {
+                                Type = "last",
+                                Value = f.Name.LastName()
+                            },
+                        }
+                    })
+                    .Generate());
+            }
+            return names;
+        }
+
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            NamesPart part = new Faker<NamesPart>()
+               .RuleFor(p => p.Names, f => GetNames(f.Random.Number(1, 3)))
+               .Generate();
+            SetPartMetadata(part, roleId, item);
+
+            return part;
+        }
+    }
+}
